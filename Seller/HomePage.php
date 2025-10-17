1e834f7c0d3d5015210a54744a550cca2855e2d1
Adithyan PM <adithyanpm745@gmail.com>
2025-10-17 16:21:54 +0530

    Change greeting from 'Hello' to 'Goodbye'

--- Seller/HomePage.php
+++ Seller/HomePage.php
@@ -598,4 +598,9 @@ body::before {
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
+
+<?php
+include('Footer.php'); 
+?>
